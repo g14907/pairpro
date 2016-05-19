42305ba70f8dcbf48890ee291bf53b4e5dba0699
g14922kh <g14922kh@s30505.tsuda.ac.jp>
2016-05-19 12:41:28 +0900

    100を追加しました。 ドライバー：g14907 ナビゲーター：Haru-haru

--- test/inttoeng/IntoToEngTestcase.java
+++ test/inttoeng/IntoToEngTestcase.java
@@ -3,6 +3,7 @@ package inttoeng;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 import org.junit.Test;
+
 public class IntoToEngTestcase {
 	@Test
 	public void translateEngで1をoneに変換() {
@@ -10,28 +11,39 @@ public class IntoToEngTestcase {
 		String actual = IntToEng.translateEng(1);
 		assertThat(actual, is(expected));
 	}
+
 	@Test
 	public void translateEngで21をtwenty_oneに変換() {
 		String expected = "twenty one";
 		String actual = IntToEng.translateEng(21);
 		assertThat(actual, is(expected));
 	}
+
 	@Test
 	public void translateEngで10をtenに変換() {
 		String expected = "ten";
 		String actual = IntToEng.translateEng(10);
 		assertThat(actual, is(expected));
 	}
+
 	@Test
 	public void translateEngで14をfourteenに変換() {
 		String expected = "fourteen";
 		String actual = IntToEng.translateEng(14);
 		assertThat(actual, is(expected));
 	}
+
 	@Test
 	public void translateEngで99をninety_nineに変換() {
 		String expected = "ninety nine";
 		String actual = IntToEng.translateEng(99);
+		assertThat(actual, is(expected));
+	}
+
+	@Test
+	public void translateEngで100をone_hundredに変換() {
+		String expected = "one hundred";
+		String actual = IntToEng.translateEng(100);
 		assertThat(actual, is(expected));
 	}
 }
